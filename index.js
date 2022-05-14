07f068ac3d251cc71e1becab6a4c4ef72297de27
Dazurashi <enaqueed@gmail.com>
2022-05-14 23:09:26 +0300

    3.21

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const generateId = () => {
     return maxId + 1
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     console.log(body.name)
     // //console.log(persons.find(person => person.name === body.name).name)
@@ -108,6 +108,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -137,6 +138,8 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    }else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
   
     next(error)
